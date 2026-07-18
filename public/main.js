632f3083ccdbbeffbc3d15af8cd04f4c7e61e321
undefined06855 <currentcashew57@gmail.com>
2026-07-18 19:05:19 +0100

    add more todos

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 // this code is so ugly
 
 // TODO: language suppot for not c++
+// TODO: proper search bar
+// TODO: maybe dont reload the whole page on page change?
+// TODO: get a better import system or whatever
+// TODO: ctrl+click support
+
 require.config({ paths: { vs: "/monaco/min/vs" } });
 require(["vs/editor/editor.main"], async () => {
     // https://microsoft.github.io/monaco-editor/typedoc/interfaces/editor_editor_api.editor.IStandaloneEditorConstructionOptions.html
